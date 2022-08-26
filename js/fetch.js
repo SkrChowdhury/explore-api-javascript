3d6862a8257ace217c08a162c70f84b48ba920bf
SkrChowdhury <sakkhor.chowdhury@gmail.com>
2022-08-27 01:36:23 +0600

    api data fetch practice

---
+++ js/fetch.js
@@ -0,0 +1,13 @@
+/* 
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+fetch(url)
+  .then((response) => response.json()) // .json is not similar but close to JSON.parse
+  .then((json) => console.log(json));
+  */
+
+  function loadData(){
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+      .then((response) => response.json()) // .json is not similar but close to JSON.parse
+      .then((json) => console.log(json));
+  }
